ad395a6ed86c731dfdf1684a586d3459d87f93f9
iamsuzon <misujon01@gmail.com>
2024-03-23 23:07:10 +0600

    wip

---
+++ bootstrap/app.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+//        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        using: function () {
+            $centralDomains = config('tenancy.central_domains');
+
+            foreach ($centralDomains as $domain) {
+                Route::middleware('web')
+                    ->domain($domain)
+                    ->group(base_path('routes/web.php'));
+            }
+
+            Route::middleware('web')->group(base_path('routes/tenant.php'));
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
